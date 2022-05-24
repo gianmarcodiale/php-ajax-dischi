50d765132b5caab96032be93a57466e7a4e8af97
gianmarcodiale <gianmarcodialessandro@gmail.com>
2022-05-24 17:55:36 +0200

    layout correction

--- milestone-1/index.php
+++ milestone-1/index.php
@@ -16,14 +16,16 @@ include __DIR__ . "/db.php";
 <body>
 
     <div class="container">
-        <div class="row">
+        <div class="row row-cols-5">
             <?php foreach ($discs as $item) : ?>
                 <?php foreach ($item as $key => $disc) : ?>
-                    <div class="card text-center">
-                        <img class="card-img-top img-fluid text-center" style="max-width:200px" src="<?= $disc['poster'] ?>" alt="">
-                        <div class="card-body">
-                            <h4 class="card-title"><?= $disc['title'] ?></h4>
-                            <p class="card-text"><?= $disc['author'] ?></p>
+                    <div class="col">
+                        <div class="card text-center">
+                            <img class="card-img-top img-fluid text-center" style="max-width:200px" src="<?= $disc['poster'] ?>" alt="">
+                            <div class="card-body">
+                                <h4 class="card-title"><?= $disc['title'] ?></h4>
+                                <p class="card-text"><?= $disc['author'] ?></p>
+                            </div>
                         </div>
                     </div>
                 <?php endforeach ?>
